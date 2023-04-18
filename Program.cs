c62672a9a291c03f2d98cc2e4b27e24162b0b3de
tech26z <2446821304@qq.com>
2023-04-18 22:41:17 +0800

    Add files via upload
    This is  source code for the main class. no other files/classes required. open it at any version of VS, then build and run it.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Elevator
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int floor;
+            Random a = new Random();
+            int currentFloor = a.Next() % 20;
+            Console.WriteLine("Floor " + currentFloor);
+            Console.WriteLine("Enter your choice:");
+            floor = Convert.ToInt32(Console.ReadLine());
+            
+            while (floor != currentFloor)
+            {
+                if(floor > currentFloor)
+                { 
+                    currentFloor++;
+                }
+                else
+                {
+                    currentFloor--;
+                }
+                
+            }
+            Console.WriteLine("You arrived floor " + currentFloor);
+            Console.ReadKey();
+        }
+       
+    }
+}
